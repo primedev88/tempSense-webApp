974cf2929851d628625bab76f06f3cb0a05aa8a1
primedev88 <119492906+primedev88@users.noreply.github.com>
2023-11-06 01:07:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 fetch('./dataset.json')
 .then(response => response.json())
 .then(data => {
-    const temp = data.data[0].Temperature;
+    const temp = data.Temperature;
     //console.log(temp)
     const temp_display=document.querySelector('#temp-val');
     temp_display.innerHTML=temp;
 
-    const volt=data.data[0].Voltage;
+    const volt=data.Voltage;
     //console.log(volt)
     const volt_display=document.querySelector('#volt-val');
     volt_display.innerHTML=volt;
 
-    const res=data.data[0].Resistance;
+    const res=data.Resistance;
     //console.log(res)
     const res_display=document.querySelector('#res-val');
     res_display.innerHTML=res;
